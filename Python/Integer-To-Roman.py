62a4e93e2b0f71d156f408ae56cd9d8cee3dce63
irmowan <irmowan@gmail.com>
2015-12-13 13:01:06 +0800

    Better solution for Integer-To-Roman

--- Python/Integer-To-Roman.py
+++ Python/Integer-To-Roman.py
@@ -11,39 +11,13 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
-        Roman = {"I":1, "V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        Roman = {1:"I", 4:"IV", 5:"V", 9:"IX", 10:"X", 40:"XL", 50:"L", 90:"XC", 100:"C", 400:"CD", 500:"D", 900:"CM", 1000:"M"}
+        keyset = sorted(Roman.keys())
         s = ""
-        n1 = num / 1000;
-        n2 = (num - n1 * 1000) / 100
-        n3 = (num / 10) % 10
-        n4 = num % 10
-        s += n1 * "M"
-        if n2 == 4:
-            s += "CD"
-        elif n2 == 9:
-            s += "CM"
-        else:
-            if n2 >= 5:
-                s += "D"
-            s += (n2 % 5) * "C"
-        # tens digit
-        if n3 == 4:
-            s += "XL"
-        elif n3 == 9:
-            s += "XC"
-        else:
-            if n3 >= 5:
-                s += "L"
-            s += (n3 % 5) * "X"
-        # last digit
-        if n4 == 4:
-            s += "IV"
-        elif n4 == 9:
-            s += "IX"
-        else:
-            if n4 >= 5:
-                s += "V"
-            s += (n4 % 5) * "I"
+        for key in reversed(keyset):
+            while num/key > 0:
+                num -= key
+                s += Roman[key]
         return s
 
 
