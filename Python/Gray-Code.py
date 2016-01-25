c989a0eae0298e79bf7b9f8bf92c5e55e38f1f25
irmowan <irmowan@gmail.com>
2016-01-26 00:06:06 +0800

    New solution: Gray-Code

---
+++ Python/Gray-Code.py
@@ -0,0 +1,37 @@
+# Time: O(2^n)
+# Space: O(1)
+
+# The gray code is a binary numeral system where two successive values differ in only one bit.
+#
+# Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code. A gray code sequence must begin with 0.
+#
+# For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+#
+# 00 - 0
+# 01 - 1
+# 11 - 3
+# 10 - 2
+# Note:
+# For a given n, a gray code sequence is not uniquely defined.
+#
+# For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+#
+# For now, the judge is able to judge based on one instance of gray code
+# sequence. Sorry about that.
+
+
+class Solution(object):
+
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans = [0]
+        for i in range(n):
+            ans = ans + [x + pow(2,i) for x in reversed(ans)]
+        return ans
+
+if __name__ == '__main__':
+    print Solution().grayCode(2)
+    print Solution().grayCode(4)
