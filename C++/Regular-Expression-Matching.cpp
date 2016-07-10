49a661ce1ef3075e4a84d9f3d68f1e1068f16380
irmowan <irmowan@gmail.com>
2016-07-10 17:43:08 +0800

    Code Style Modification

--- C++/Regular-Expression-Matching.cpp
+++ C++/Regular-Expression-Matching.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
+    bool isMatch(string s, string p)
+    {
         int l = s.size();
-        vector <int> t(l + 1, 0);
+        vector<int> t(l + 1, 0);
+        vector<int> tt(l + 1, 0);
         t[0] = 1;
         int pp = 0;
         while (pp < p.size()) {
             // Print
-            for (auto i = 0; i < l + 1; ++i) cout << t[i];
-            cout << endl;
+            // for (auto i = 0; i < l + 1; ++i)
+            //     cout << t[i];
+            // cout << endl;
 
             bool star = false;
             char c = p[pp];
             pp++;
             if (pp < p.size() && p[pp] == '*') {
                 pp++;
-                star=true;
+                star = true;
             }
-            cout << c << ' ' << star << endl;
-            vector <int> tt(l + 1, 0);
+            // cout << c << ' ' << star << endl;
+            fill(tt.begin(), tt.end(), 0);
             for (int i = 0; i < l + 1; ++i) {
-                if (t[i] == 0) continue;
+                if (t[i] == 0)
+                    continue;
                 if (star == false) {
-                    if (i < l && (s[i] == c || c == '.')) tt[i + 1] = 1;
-                }
-                else {
+                    if (i < l && (s[i] == c || c == '.'))
+                        tt[i + 1] = 1;
+                } else {
                     auto k = i;
                     tt[k] = 1;
                     while (k < l && (s[k] == c || c == '.')) {
@@ -36,10 +40,12 @@ public:
             t = tt;
         }
         // Print
-        for (auto i = 0; i < l + 1; ++i) cout << t[i];
-        cout << endl;
+        // for (auto i = 0; i < l + 1; ++i)
+        //     cout << t[i];
+        // cout << endl;
 
-        if (t[l] == 1) return true;
+        if (t[l] == 1)
+            return true;
         return false;
     }
 };
