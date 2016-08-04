f91a0a22121f8fc6c554e0fa8291be53e544b307
irmowan <irmowan@gmail.com>
2016-08-04 23:45:15 +0800

    New solution: Course-Schedule-II

---
+++ C++/Course-Schedule-II.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+  vector<int> findOrder(int numCourses, vector<pair<int, int>> &prerequisites) {
+    vector<int> degree(numCourses, 0);
+    queue<int> zeroPoint;
+    vector<int> result;
+    for (pair<int, int> prerequisite : prerequisites) {
+      degree[prerequisite.first]++;
+    }
+    for (int i = 0; i < numCourses; ++i) {
+      if (degree[i] == 0)
+        zeroPoint.push(i);
+    }
+    while (!zeroPoint.empty()) {
+      int key = zeroPoint.front();
+      zeroPoint.pop();
+      result.push_back(key);
+      for (pair<int, int> prerequisite : prerequisites) {
+        if (prerequisite.second == key) {
+          if (--degree[prerequisite.first] == 0) {
+            zeroPoint.push(prerequisite.first);
+          };
+        }
+      }
+    }
+    if (result.size() < numCourses) {
+      result.clear();
+    }
+    return result;
+  }
+};
