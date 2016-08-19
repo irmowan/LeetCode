ec327c41ca58ce25f3c7175347581ebb6befd690
irmowan <irmowan@gmail.com>
2016-08-20 00:40:30 +0800

    Add solution: Power-of-Three

--- C++/Power-of-Three.cpp
+++ C++/Power-of-Three.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
   bool isPowerOfThree(int n) {
-    if (n > 1)
+    if (n > 1) {
       while (n % 3 == 0)
         n /= 3;
+    }
     return n == 1;
+  }
+  bool isPowerOfThree_2(int n) {
+    double l = log10(n) / log10(3);
+    return ((l - int(l)) == 0) ? true : false;
   }
 };
