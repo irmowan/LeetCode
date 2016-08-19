84e8dab110d0a1727332a1c15bccc96dc7a35d1f
irmowan <irmowan@gmail.com>
2016-08-20 00:16:42 +0800

    New solution: Power-of-Four

---
+++ C++/Power-of-Four.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+  bool isPowerOfFour(int num) {
+    if (num <= 0)
+      return false;
+    if ((num & (num - 1)) == 0 && (num & 0x55555555) > 0)
+      return true;
+    return false;
+  }
+};
